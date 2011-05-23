3b097ec9f3a730d75a666e57d06e3b8a1283347a
Yuriy Mykytyuk <myk.yura@gmail.com>
2011-05-23 15:42:02 +0300

    create include file

--- Resources/includes/webserviceclient.js
+++ Resources/includes/webserviceclient.js
@@ -1,12 +1,4 @@
-//var serviceURL = "https://betaapi.helpdesk.bigwebapps.com/";
-
-var serviceURL = "https://192.168.0.46/bigWebApps.HelpDesk.WebApi.Server/";
-
-var serviceLogin = "use@example.com";
-
-var servicePassword = "password";
-
-var validateCertificate = false;
+Ti.include('include.js');
 
 function mbl_dataExchange(requestType, requestURL, onload, ondatastream, onerror) {
 	mbl_dataExchange(requestType, requestURL, onload, ondatastream, onerror, null);
@@ -27,7 +19,7 @@ function mbl_dataExchange(requestType, requestURL, onload, ondatastream, onerror
     	loader.onerror = onerror;
     Ti.API.info('requestContent=' + requestContent);
     loader.setRequestHeader("Content-Type", "application/rss+xml");
-    loader.setRequestHeader("Authorization", "Basic " + Titanium.Utils.base64encode(serviceLogin + ":" + servicePassword));
+    loader.setRequestHeader("Authorization", authorizationData);
     if (requestContent == null)
     	loader.send();
     else
