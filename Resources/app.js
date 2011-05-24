64327410c059e3e1ec15a560671363d2bfbea929
Yuriy Mykytyuk <myk.yura@gmail.com>
2011-05-24 20:41:34 +0300

    add base of global notification handler

--- Resources/app.js
+++ Resources/app.js
@@ -76,3 +76,90 @@ var winHome = Ti.UI.createWindow({
 navGroup.window = winHome;
 appBase.add(navGroup);
 appBase.open(); 
+
+(function()
+{ 
+	Ti.App.addEventListener("show_complete_message", function(e) {
+		// window container
+		var modalWindow = Titanium.UI.createWindow({
+			height:80,
+			width:200,
+			touchEnabled:false
+		});
+
+		// black view
+		var modalView = Titanium.UI.createView({
+			height:80,
+			width:200,
+			backgroundColor:'#000',
+			borderRadius:10,
+			opacity:0.8,
+			touchEnabled:false
+		});
+		modalWindow.add(modalView);
+
+		// message
+		var modalMessage = Titanium.UI.createLabel({
+			text: e.labelText,
+			color:'#fff',
+			textAlign:'center',
+			font:{fontSize:18,fontWeight:'bold'},
+			height:'auto',
+			width:'auto'
+		});
+		modalWindow.add(modalMessage);
+		
+
+		modalWindow.open();
+
+		var animationProperties = {delay: 1500, duration: 5000, opacity: 0.1};
+		if (Ti.Platform.osname == "iPhone OS") {
+			animationProperties.transform =
+				Ti.UI.create2DMatrix().translate(-200,200).scale(0);
+		}
+		modalWindow.animate(animationProperties, function()
+		{
+			modalWindow.close();
+		});
+	});
+})();
+
+/*// Global Event Indicator
+var messageWin = Titanium.UI.createWindow({
+	height:30,
+	width:250,
+	bottom:70,
+	borderRadius:10,
+	touchEnabled:false,
+
+	orientationModes : [
+	Titanium.UI.PORTRAIT,
+	Titanium.UI.UPSIDE_PORTRAIT,
+	Titanium.UI.LANDSCAPE_LEFT,
+	Titanium.UI.LANDSCAPE_RIGHT,
+	]
+});
+var messageView = Titanium.UI.createView({
+	id:'messageview',
+	height:30,
+	width:250,
+	borderRadius:10,
+	backgroundColor:'#000',
+	opacity:0.7,
+	touchEnabled:false
+});
+
+var messageLabel = Titanium.UI.createLabel({
+	id:'messagelabel',
+	text:'',
+	color:'#fff',
+	width:250,
+	height:'auto',
+	font:{
+		fontFamily:'Helvetica Neue',
+		fontSize:13
+	},
+	textAlign:'center'
+});
+messageWin.add(messageView);
+messageWin.add(messageLabel);*/
