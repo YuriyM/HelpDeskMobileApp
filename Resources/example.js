d8cd982bb066665103bec6556bb22a9275c89261
Yuriy Mykytyuk <myk.yura@gmail.com>
2011-05-25 17:32:46 +0300

    remove old app.js files

--- Resources/example.js
+++ Resources/example.js
@@ -70,3 +70,80 @@ h.addEventListener('blinkComplete', function() {
     Ti.API.info('blink complete');
 });
 h.blink(); // will cause the view to fade out, then in*/
+
+//----------------------
+// OLD APP.JS - TABGROUP IMPLEMENTATION + JSS FILE
+//----------------------
+/*// set bg color of master view
+Titanium.UI.setBackgroundColor('#000');
+
+// Create general tabgroup
+
+var tabGroupHome = Titanium.UI.createTabGroup({ id: 'tgHome' });
+
+// Create window for Home tab
+var winHome = Titanium.UI.createWindow({ id: 'winHome',  url: 'home/home.js' });
+
+var tabHome = Titanium.UI.createTab({    
+    id: 'tabHome',
+	window: winHome   
+});
+
+// Window for Tickets
+var winTickets = Titanium.UI.createWindow({ id: 'winTickets',  url: 'tickets/ticket_list.js'});
+
+var tabTickets = Titanium.UI.createTab({
+	id: 'tabTickets',
+    window: winTickets
+});
+
+// Window for Assets 
+var winAssets = Titanium.UI.createWindow({ id: 'winAssets', url: 'assets/tweets.js' });
+
+var tabAssets = Titanium.UI.createTab({
+    id: 'tabAssets',
+    window: winAssets
+});
+
+
+// add tabs
+tabGroupHome.addTab(tabHome);
+tabGroupHome.addTab(tabTickets);
+tabGroupHome.addTab(tabAssets);
+
+tabGroupHome.addEventListener('open', function () {// set background color back to white after tab group transition
+    Titanium.UI.setBackgroundColor('#fff');
+});
+
+
+tabGroupHome.setActiveTab(0);
+
+tabGroupHome.open();*/
+/*
+#tabHome {
+    icon: 'images/KS_nav_ui.png';
+    title: "Home";    
+}
+
+#winHome {    
+    title: "bigWebApps HelpDesk";    
+}
+
+#winTickets {    
+    title: 'Tickets';
+}
+
+#tabTickets {
+    icon: 'images/KS_nav_views.png';    
+    title: "Tickets";
+    height: 16;    
+}
+
+#winAssets {    
+    title: 'Assets';
+}
+
+#tabAssets {
+    icon: 'images/KS_nav_ui.png';   
+    title: "Assets";    
+}*/
